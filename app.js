acccf29eb81d657c836fb51a4d64dfe26ec3f6af
Dorota S <dorota.stefanik@gmail.com>
2023-09-22 17:12:28 +0200

    rgb & hex version - delete comments & console.log

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-// const colors = ['green', 'red', 'rgb(133, 122, 200)', '#f15025'];
-
 const btn = document.getElementById('btn');
-const color = document.querySelector('.color');  // span
+const color = document.querySelector('.color');
 
-btn.addEventListener('click', () => {  // zmiana background color
-  // get random number between 0 - 255
+btn.addEventListener('click', () => {
+  
   const r = getRandomNumber();
   const g = getRandomNumber();
   const b = getRandomNumber();
   const rgb = `rgb(${r}, ${g}, ${b})`;
-  console.log(r);
-  console.log(g);
-  console.log(b);
+  
 
-  document.body.style.backgroundColor = rgb;  // wbudowane property style .backgroundColor odczytuje stringa jako kolor
+  document.body.style.backgroundColor = rgb;
   color.textContent = rgb;
 });
 
+// get random number between 0 - 255
 function getRandomNumber() {
   return Math.floor(Math.random() * 256);
 }
--- hex.js
+++ hex.js
@@ -1,11 +1,10 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];  // system szesnastkowy, A reprezentuje 10, B-11, C-12, D-13, E-14, F-15
- // #f15025  -> hasztag + 6 values
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', () => {
-  let hexColor = '#';  // ponieważ jest tu string - kolejne liczby nie będą sie dodawały tylko dopisywały
+  let hexColor = '#';
   for(let i = 0; i < 6; i++) {
     hexColor += hex[getRandomNumber()];
   }
@@ -16,5 +15,3 @@ btn.addEventListener('click', () => {
 function getRandomNumber() {
   return Math.floor(Math.random() * hex.length);
 }
-
-
