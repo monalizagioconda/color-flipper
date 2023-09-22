8fcd644bde2a2a81ab991210d6b029d2f755083c
Dorota S <dorota.stefanik@gmail.com>
2023-09-22 16:51:29 +0200

    rgb-version

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-const colors = ['green', 'red', 'rgb(133, 122, 200)', '#f15025'];
+// const colors = ['green', 'red', 'rgb(133, 122, 200)', '#f15025'];
 
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');  // span
 
 btn.addEventListener('click', () => {  // zmiana background color
-  // get random number between 0 - 3
-  const randomNumber = getRandomNUmber();
-  console.log(randomNumber);
+  // get random number between 0 - 255
+  const r = getRandomNumber();
+  const g = getRandomNumber();
+  const b = getRandomNumber();
+  const rgb = `rgb(${r}, ${g}, ${b})`;
+  console.log(r);
+  console.log(g);
+  console.log(b);
 
-  document.body.style.backgroundColor = colors[randomNumber];  // wbudowane property style .backgroundColor odczytuje stringa pod indexem [x] jako kolor
-  color.textContent = colors[randomNumber];
+  document.body.style.backgroundColor = rgb;  // wbudowane property style .backgroundColor odczytuje stringa jako kolor
+  color.textContent = rgb;
 });
 
-function getRandomNUmber() {
-  return Math.floor(Math.random() * colors.length);
+function getRandomNumber() {
+  return Math.floor(Math.random() * 256);
 }
